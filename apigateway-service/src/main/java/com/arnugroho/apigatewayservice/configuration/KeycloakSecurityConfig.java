8e24d34687d1ec82bfb63fb7c9cd18650045fc4b
arnugroho <nugroho.akbar.r@gmail.com>
2021-10-10 19:37:50 +0700

    update KeycloakSecurityConfig.java

--- apigateway-service/src/main/java/com/arnugroho/apigatewayservice/configuration/KeycloakSecurityConfig.java
+++ apigateway-service/src/main/java/com/arnugroho/apigatewayservice/configuration/KeycloakSecurityConfig.java
@@ -8,14 +8,12 @@ import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @KeycloakConfiguration
-@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
     @Override
