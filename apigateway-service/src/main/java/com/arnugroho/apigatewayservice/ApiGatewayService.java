5e8b8181c1129dc480a8eb8bbb43d16e88b039e8
arnugroho <nugroho.akbar.r@gmail.com>
2021-10-13 22:59:36 +0700

    add route

--- apigateway-service/src/main/java/com/arnugroho/apigatewayservice/ApiGatewayService.java
+++ apigateway-service/src/main/java/com/arnugroho/apigatewayservice/ApiGatewayService.java
@@ -2,10 +2,23 @@ package com.arnugroho.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayService {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayService.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/hello")
+                        //.filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://localhost:1414"))
+                .build();
     }
 }
